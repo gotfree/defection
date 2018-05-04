bdb12778e85397c23e7df2e474e74ee0a64d72d6
RobKano <kanosan77@gmail.com>
2018-05-04 15:16:39 +0400

    Some pandas (from work)

--- output_write.py
+++ output_write.py
@@ -1,5 +1,5 @@
 """Модуль функций отображения и записи информации в файл
-"""
+
 import os
 
 cwd = os.getcwd()
@@ -8,3 +8,13 @@ print(cwd)
 
 for i in file_lst:
     print(i)
+"""
+import pandas as pd
+from openpyxl import load_workbook
+
+wb = load_workbook('src/sheet.xlsx')
+sheet = wb.get_sheet_by_name('Лист1')
+
+df = pd.DataFrame(sheet.values)
+
+print(df)
